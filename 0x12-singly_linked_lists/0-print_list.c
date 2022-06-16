9a3b752e05b36149c436b15ba3b4991d2390cfff
BabaMicah <sammiebwoysamuels67@gmail.com>
2022-06-16 09:55:53 -0700

    task 0 fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include "lists.h"
- /**
+/**
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
  * Return: number of elements in the list.
@@ -19,6 +19,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		nelem++;
 	}
-
 	return (nelem);
 }
